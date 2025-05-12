be25fc4f8b6e1d2c6e57693c2963d543a3ef7b35
Todd Hardeman <toddhardeman6@gmail.com>
2025-05-12 08:46:51 -0700

    added cookies that track my name

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
 const port = 8080;
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 }
 );
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const name = req.cookies.name;
+    if (name) {
+        res.send(`Welcome ${name}!`);
+    } else {
+        res.send('Welcome! Please set your name by visiting /login?name=YourName');
     }
+});
+
+app.get('/login', (req, res) => {
+    const name = req.query.name;
+    if (name) {
+        res.cookie('name', name, {
+            httpOnly: true,
+        });
+        res.send(`Welcome ${name}! your name has been saved in a cookie.`);
+    } 
+    else 
+    {
+        res.send('request is not working')
+    }
+
+}
 );
